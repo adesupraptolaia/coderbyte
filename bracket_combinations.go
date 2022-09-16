0e9479cddd78f1a06c82c2fce3a9ca11aef4076c
Ade Suprapto Laia Putra <101562563+ade-kargo@users.noreply.github.com>
2022-09-16 15:04:25 +0700

    update bracket_combination solution

--- bracket_combinations.go
+++ bracket_combinations.go
@@ -2,9 +2,9 @@ package main
 
 import "fmt"
 
-func factorial(n int) int {
+func multiply(n, m int) int {
 	result := 1
-	for n > 0 {
+	for n >= m {
 		result *= n
 		n -= 1
 	}
@@ -13,16 +13,27 @@ func factorial(n int) int {
 }
 
 // (n k) = n!/k!(n-k!)
+// = n*(n-1)*...*(max+1) / min*(min-1)*...*1
 func combination(n, k int) int {
-	return factorial(n) / (factorial(k) * factorial(n-k))
+	max := n - k
+	min := k
+	if k > n-k {
+		max = k
+		min = n - k
+	}
+
+	return multiply(n, max+1) / multiply(min, 1)
 }
 
-// (2n n) ==> all of posibility, 
+// (2n n) ==> all of posibility,
 // actualy 2n!/n!n! ==>  because you have 2n brackets, and you have n open_bracket and n close_bracket
 // ===============
 // (2n n+1) ==> all of bad bracket_combinations
 // you just need take n+1 bracket (from 2n bracket) to get bad bracket_combination
 func BracketCombinations(num int) int {
+	if num == 0 {
+		return 1
+	}
 	return combination(2*num, num) - combination(2*num, num+1)
 }
 
